a0d1012d8596d41b60d39d334d305b8ca300bda1
leandjb <leandjb@gmail.com>
2023-01-04 10:00:27 -0500

    implements assertion for Mock on API response

--- src/test/java/com/leandjb/demo/controllers/UserControllerTest.java
+++ src/test/java/com/leandjb/demo/controllers/UserControllerTest.java
@@ -1,7 +1,9 @@
 package com.leandjb.demo.controllers;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.leandjb.demo.models.User;
 import com.leandjb.demo.services.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +13,9 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.event.annotation.BeforeTestExecution;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 
@@ -33,5 +37,21 @@ public class UserControllerTest {
 
         User user = controller.get(null, "31");
         verify(service).get("31");
+    }
+    @Test
+    public void getUserObjectTest(){
+        User userMock = new User();
+        userMock.setEmail("test@test.com");
+        userMock.setId("31");
+        userMock.setName("nombre");
+        userMock.setPassword("clave");
+
+        when(service.get(any())).thenReturn(userMock);
+
+        User user = controller.get(null, "31");
+        verify(service).get("31");
+
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("31", user.getId());
     }
 }
---
+++ src/test/java/com/leandjb/demo/services/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.leandjb.demo.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    private  UserService service;
+
+    @BeforeTestExecution
+    public void setup(){
+        initMocks(this);
+    }
+
+    @Test
+    public void getUserTest(){
+
+    }
+}
